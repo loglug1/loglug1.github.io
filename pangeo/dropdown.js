319aa5fd23e1819c8d808a01b1ff6fac48d24046
Camden McKay <62907417+loglug1@users.noreply.github.com>
2023-09-19 13:58:15 -0500

    Delete pangeo directory

--- pangeo/dropdown.js
+++
@@ -1,8 +0,0 @@
-function showSharks(){
-    var rn = document.getElementById("dropdown");
-    if (rn.style.display === "block"){
-        rn.style.display = "none";
-    } else {
-        rn.style.display = "block";
-    }
-}
